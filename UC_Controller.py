bc29cef23cd29a84d5b76ad6fe863a090e68c260
Benjamin D Fulker <bdfulker2@hotmail.com>
2019-11-11 13:10:14 -0500

    Update UC_Controller.py

--- UC_Controller.py
+++ UC_Controller.py
@@ -1,13 +1,13 @@
-import smtplib       #email lib in email_ip_address()
-from email.MIMEMultipart import MIMEMultipart #for email
-from email.MIMEText import MIMEText           #for email
+#import smtplib       #email lib in email_ip_address() 
+#from email.MIMEMultipart import MIMEMultipart #for email
+#from email.MIMEText import MIMEText           #for email
 import sys           #sys
 import os            #os
 import random
 import urllib        #url lib in check_internet_connect()
 import time          #time
 import socket
-import bluetooth     #bluetooth lib used in main()
+#import bluetooth     #bluetooth lib used in main()
 import Adafruit_GPIO.SPI as SPI     #display and pi
 import Adafruit_SSD1306             #display lib
 from PIL import Image      #for display
@@ -35,7 +35,7 @@ disp = Adafruit_SSD1306.SSD1306_128_64(
 
 disp.begin()        #display begin
 
-time.sleep(3)
+time.sleep(1)
 
 disp.clear()                #clear display
 disp.display()              #display the display
@@ -45,71 +45,57 @@ image = Image.new('1', (width, height)) #create image
 draw = ImageDraw.Draw(image)#draw the image from above
 
 font = ImageFont.load_default()     #set display font
-ellipsis = ".   "           
-count = 0                   #initialize count to 0
 
 v_status = False 
 cs_status = False 
 t_status = False
 
 def Client():
-    
-    # create TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    status = False                                             #set local status to false
+    #create socket AF_INET refers to ipv4 and SOCK_STREAM means connection is TCP
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #
+#    client.connect(('0,0,0,0', 8080))
+    client.connect(('10.0.0.55', 6000))     #connect to server ip on port 6000
 
-    # retrieve local hostname
-    local_hostname = socket.gethostname()
+    client.send("Server Start\n")           #send start command to server
 
-    # get fully qualified hostname
-    local_fqdn = socket.getfqdn()
-
-    # get the according IP address
-    ip_address = socket.gethostbyname(local_hostname)
-
-    # bind the socket to the port 23456, and connect
-    server_address = (ip_address, 23456)
-    sock.connect(server_address)
-    print("connecting to %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
-
-    # define example data to be sent to the server
-    passenger_data = ["true", "false", "false", "true", "false", "true"]
-    for entry in passenger_data:
-        print("is there a passenger: %s" % entry)
-        new_data = str("Passenger: %s\n" % entry).encode("utf-8")
-        sock.sendall(new_data)
-
-        # wait for two seconds
-        time.sleep(2)
-
-    # close connection
-    sock.close()
-
-    return True
+    from_server = client.recv(4096)         #receive data from server
+    if(from_server == "True"):              #if from server is "True"
+        status = True                       #assign status to True
+    else:                                   #else from_server = "False"
+        status = False                      #assign status to False
+    client.close()                          #socket close
+                                            #print to console
+    print "return value from server = " + from_server      
+    return status                           #return bool status value
 
 
+#Timer() uses time library to sleep for 1 second on each iteration counting down
+#from 29.
+#parameter: int my_timer tells the timer weather its a horn timer or
 def Timer(my_timer):
-    for sec in range(29,0,-1):
+    for sec in range(29,0,-1):             
         time.sleep(1)
-        
+
         if(my_timer == 1):
-            #print_oled("Time Left = ", str(sec), " seconds.", True)
+            print_oled("Time Left = ", str(sec), " seconds.", True)
             print(str(sec))
         else:
-            #print_oled("Horn on ", str(sec), " seconds.", True)
+            print_oled("Horn on ", str(sec), " seconds.", True)
             print("Horn Timer = " + str(sec))
     return True
   
 
-
+#Starts the horn and calls the timer() to countdown 30 seconds and terminates the horn
 def Horn():
-    #print_oled("", "Horn On" , "" , False)
+    print_oled("", "Horn On" , "" , False)
     print("Horn on")
     Timer(2)
-    #print_oled("", "Horn Off" , "" , False)
+    print_oled("", "Horn Off" , "" , False)
     print("Horn off")
     
 
-
+#method for printing to the oled screen. Sets parameters and creates 3 lines of text
 def print_oled(line_one, line_two, line_three, no_sleep):
     draw.rectangle((0, 0, width, height), outline=0, fill = 0)
     draw.text((0, 12), line_one, font=font, fill=255)
@@ -122,51 +108,44 @@ def print_oled(line_one, line_two, line_three, no_sleep):
         time.sleep(5)
     return
 
-#The check_internet_connect method try's to check if there is Wi-Fi connection 
-#to a specified URL to verify that it is connect to the internet 
-def check_internet_connect(macAddress):   #method to check internet connection
-    try:
-        url = "https://www.google.com/"     #using google.com as url to test
-        urllib.urlopen(url)                 #uses urllib to open a url
-        status = "Connected"                #assign status = connected
-    except urllib2.URLError, e:             #exception URLError
-        status = "Not connected"            #if url error status = not connected
-    
-    print status                            #print status to console
-    if  (status == "Connected"):            #if status is still connected
-        mail_mac_address(macAddress)        #call to mail_mac_address
-                                            #with parameter of macAddress
-    return
-
-
+#main method __init__
+#parameter: bool vehicle. Passed on/off ignition status of the vehicle
 def __init__(vehicle): 
       
 
     if(vehicle == False):
-        #print_oled("Vehicle", "Turned", "off", False)
-        print("Vehicle " + " Turned" +" off")
+        print_oled("Vehicle", "Turned", "off", False)         #print oled
+        print("Vehicle " + " Turned" +" off")                 #print console
     else:
-        #print_oled("Vehicle", "Still", "On", False)
-        #print_oled("System", "Shut", "down", False)
-        print("Vehicle " + " Still" +" On")
-        print("Shut down")
-        raise SystemExit()
+        print_oled("Vehicle", "Still", "On", False)           #print oled
+        print_oled("System", "Shut", "down", False)           #print oled
+        print("Vehicle " + " Still" +" On")                   #print console
+        print("Shut down")                                    #print console
+        raise SystemExit()                           #system shutdown if vehichle is on
     
-    if(Client()):
+    if(Client()):        
         t_status = Timer(1)
-        print(str(t_status))
+        print("t_status = " + str(t_status))
     else:
-        #print_oled("Car", "Still", "On", False)
-        #print_oled("System", "Shut", "down", False)
-        print("Vehicle " + " Still" +" On")
+        print_oled("Car Seat", "Was", "Unoccupied", False)    #print oled
+        print_oled("System", "Shut", "down", False) 
+        print("Car Seat " + " Was" +" Unoccupied")            #print console
         print("Shut down")
-        raise SystemExit()
+        raise SystemExit()                          #system shutdown if vehichle is on
     
     if(t_status):
-        cs_status = Client()
+        cs_status = Client()      #2nd call to Client() to check if car seat is occupied
 
-    if(cs_status):
-        t_status = False
-        Horn()
+    if(cs_status):                                         
+        t_status = False                                      #reset t_status
+        Horn()                                                
+    else:
+        print_oled("Car Seat", "Is Now", "Unoccupied", False) #print oled
+        print_oled("System", "Shut", "down", False)
+        print("Car Seat " + " Is Now" +" Unoccupied")         #print console
+        print("Shut down")                          #system shutdown if vehichle is on
+    
+    disp.clear()                                              #clear disply
+    disp.display()
 
 __init__(False)
